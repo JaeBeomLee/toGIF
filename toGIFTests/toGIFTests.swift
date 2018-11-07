31565cac3af51ac5334fe5c0cecbc2a17c691dcd
이재범 <btv9200@gmail.com>
2018-11-07 22:09:19 +0900

    Initial Commit

---
+++ toGIFTests/toGIFTests.swift
@@ -0,0 +1,34 @@
+//
+//  toGIFTests.swift
+//  toGIFTests
+//
+//  Created by 이재범 on 07/11/2018.
+//  Copyright © 2018 Jay_Lee. All rights reserved.
+//
+
+import XCTest
+@testable import toGIF
+
+class toGIFTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
